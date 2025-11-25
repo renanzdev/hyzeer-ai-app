6cd214400bdfb1879b6e2840d723460a57148cc0
renanzdev <renangd.dev@gmail.com>
2025-11-25 17:29:01 -0300

    Initial Commit

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Provider from "./provider";
 
 export const metadata = {
   title: "Create Next App",
@@ -18,11 +9,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
+       
       </body>
     </html>
   );
