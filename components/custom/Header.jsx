ee9bf4584a7a31a63e9e1bd305e3896ee94a285a
renanzdev <renangd.dev@gmail.com>
2025-11-28 18:46:22 -0300

    feat: atualiza gradiente de fundo e ajusta tamanho do logo no cabeçalho

--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -6,7 +6,7 @@ import Colors from '@/data/Colors'
 function Header() {
   return (
     <div className='p-4 flex justify-between items-center'>
-        <Image src='/logo.png' alt='Logo' width={40} height={40} />
+        <Image src='/logo.png' alt='Logo' width={150} height={40} />
         <div className='flex gap-5'>
             <Button variant='ghost'>Sign In</Button>
             <Button className='text-white' style={{
